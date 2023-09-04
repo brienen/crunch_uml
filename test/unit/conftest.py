6ff9cd4954e8eb41ba6c329b756ec298466330f8
Arjen Brienen <brienen@e-space.nl>
2023-09-04 14:27:59 +0200

    Fix Output dir

--- test/unit/conftest.py
+++ test/unit/conftest.py
@@ -1,6 +1,17 @@
+import os
+import shutil
 import pytest
-
 
 @pytest.fixture
 def mock_function():
     pass
+
+
+@pytest.fixture(scope="session", autouse=True)
+def test_setup_output_directory():
+    # Voorbereidingscode: Maak de directory aan
+    output_dir = './test/output'
+    os.makedirs(output_dir, exist_ok=True)
+    yield  # Hier voeren de tests uit
+    # Opruimcode: Verwijder de directory na de tests
+    shutil.rmtree(output_dir)
