c1c8c45dc8f2c69cdc5688732cca1c2d0755aa29
Arjen Brienen <brienen@e-space.nl>
2024-09-26 18:10:12 +0200

    last Lod addition shacl next

--- crunch_uml/renderers/lodrenderer.py
+++ crunch_uml/renderers/lodrenderer.py
@@ -17,13 +17,19 @@ logger = logging.getLogger()
 class LodRenderer(ModelRenderer):
     """
     Renders all model packages using jinja2 and a template.
-    A model package is a package with at least 1 class inside
+    A model package is a package with at least 1 class inside.
+    This renderer utilizes MIM for ontology representation and SHACL for validation.
     """
 
     def writeToFile(self, graph, args):
         pass
 
     def render(self, args, zchema: sch.Schema):
+        """
+        Renders the model into RDF format using the provided schema.
+        Validates the generated RDF against SHACL shapes to ensure compliance.
+        If no namespace is provided, a default is used.
+        """
         if args.linked_data_namespace is None:
             logger.warning(
                 f'No namespace provided via parameter "linked_data_namespace", using default {const.DEFAULT_LOD_NS}'
@@ -55,7 +61,7 @@ class LodRenderer(ModelRenderer):
 
             for cls in model.classes:
                 class_dict[cls.id] = ns[cls.id]
-                g.add((ns[cls.id], RDF.type, mim.Objecttype))  # Add stereotype
+                g.add((ns[cls.id], RDF.type, mim.Objecttype))  # Add stereotype for class
 
                 g.add((ns[cls.id], RDF.type, OWL.Class))
                 g.add((ns[cls.id], RDFS.label, Literal(cls.name)))
@@ -64,28 +70,28 @@ class LodRenderer(ModelRenderer):
 
                 # SHACL NodeShape for the class
                 shape_uri = ns[cls.id + "_Shape"]
-                g.add((shape_uri, RDF.type, SH.NodeShape))
-                g.add((shape_uri, SH.targetNode, ns[cls.id]))
+                g.add((shape_uri, RDF.type, SH.NodeShape))  # Define a SHACL NodeShape
+                g.add((shape_uri, SH.targetNode, ns[cls.id]))  # Target the class node
 
                 for attribute in cls.attributes:
-                    attribute_bnode = BNode()
+                    attribute_bnode = BNode()  # Create a blank node for the attribute
                     datatype = XSD.string if attribute.primitive is None else attribute.primitive
-                    g.add((ns[attribute.id], RDF.type, OWL.DatatypeProperty))
-                    g.add((ns[attribute.id], RDFS.domain, ns[cls.id]))
-                    g.add((ns[attribute.id], RDFS.label, Literal(attribute.name)))
-                    g.add((ns[attribute.id], RDFS.range, datatype))
+                    g.add((ns[attribute.id], RDF.type, OWL.DatatypeProperty))  # Define as DatatypeProperty
+                    g.add((ns[attribute.id], RDFS.domain, ns[cls.id]))  # Set domain to the class
+                    g.add((ns[attribute.id], RDFS.label, Literal(attribute.name)))  # Label for the attribute
+                    g.add((ns[attribute.id], RDFS.range, datatype))  # Define the range of the attribute
                     g.add((ns[attribute.id], Namespace("http://w3.org/ns/shacl#").datatype, datatype))  # sh:datatype
                     g.add(
                         (ns[attribute.id], Namespace("http://w3.org/ns/shacl#").path, Literal(attribute.name))
                     )  # sh:path
-                    g.add((ns[attribute.id], RDF.type, mim.Attribuutsoort))  # Add attribute stereotype
+                    g.add((ns[attribute.id], RDF.type, mim.Attribuutsoort))  # Add stereotype for attribute
 
                     # SHACL property structure
-                    g.add((shape_uri, SH.property, attribute_bnode))
-                    g.add((attribute_bnode, SH.path, ns[attribute.id]))
-                    g.add((attribute_bnode, SH.datatype, datatype))
-                    g.add((attribute_bnode, SH.minCount, Literal(1)))
-                    g.add((attribute_bnode, SH.maxCount, Literal(1)))
+                    g.add((shape_uri, SH.property, attribute_bnode))  # Link property to NodeShape
+                    g.add((attribute_bnode, SH.path, ns[attribute.id]))  # Define path for SHACL
+                    g.add((attribute_bnode, SH.datatype, datatype))  # Define datatype for SHACL
+                    g.add((attribute_bnode, SH.minCount, Literal(1)))  # Minimum count for SHACL
+                    g.add((attribute_bnode, SH.maxCount, Literal(1)))  # Maximum count for SHACL
 
                     if attribute.definitie is not None:
                         g.add((
@@ -104,7 +110,7 @@ class LodRenderer(ModelRenderer):
                         sub_cls = class_dict.get(subclass.superclass.id)
 
                         if super_cls is not None and sub_cls is not None:
-                            g.add((sub_cls, RDFS.subClassOf, super_cls))
+                            g.add((sub_cls, RDFS.subClassOf, super_cls))  # Define subclass relationship
 
                 # Then set associations
                 for assoc in cls.uitgaande_associaties:
@@ -112,10 +118,10 @@ class LodRenderer(ModelRenderer):
                     to_cls = class_dict.get(assoc.dst_class.id)
 
                     if from_cls is not None and to_cls is not None:
-                        g.add((ns[assoc.id], RDF.type, OWL.ObjectProperty))
-                        g.add((ns[assoc.id], RDFS.domain, from_cls))
-                        g.add((ns[assoc.id], RDFS.range, to_cls))
-                        g.add((ns[assoc.id], RDFS.label, Literal(assoc.name)))
+                        g.add((ns[assoc.id], RDF.type, OWL.ObjectProperty))  # Define as ObjectProperty
+                        g.add((ns[assoc.id], RDFS.domain, from_cls))  # Set domain for association
+                        g.add((ns[assoc.id], RDFS.range, to_cls))  # Set range for association
+                        g.add((ns[assoc.id], RDFS.label, Literal(assoc.name)))  # Label for the association
                         g.add((ns[assoc.id], RDF.type, mim.Relatiesoort))  # Add stereotype for relationship
                         if assoc.definitie is not None:
                             g.add((ns[assoc.id], RDFS.comment, Literal(assoc.definitie)))
@@ -140,8 +146,8 @@ class LodRenderer(ModelRenderer):
 )
 class TTLRenderer(LodRenderer):
     """
-    Renders all model packages using jinja2 and a template.
-    A model package is a package with at least 1 class inside
+    Renders Linked Data ontology in Turtle format.
+    A model package is a package with at least 1 class inside.
     """
 
     def writeToFile(self, graph, args):
@@ -161,8 +167,8 @@ class TTLRenderer(LodRenderer):
 )
 class RDFRenderer(LodRenderer):
     """
-    Renders all model packages using jinja2 and a template.
-    A model package is a package with at least 1 class inside
+    Renders Linked Data ontology in RDF format.
+    A model package is a package with at least 1 class inside.
     """
 
     def writeToFile(self, graph, args):
@@ -182,8 +188,8 @@ class RDFRenderer(LodRenderer):
 )
 class JSONLDRenderer(LodRenderer):
     """
-    Renders all model packages using jinja2 and a template.
-    A model package is a package with at least 1 class inside
+    Renders Linked Data ontology in JSON-LD format.
+    A model package is a package with at least 1 class inside.
     """
 
     def writeToFile(self, graph, args):
