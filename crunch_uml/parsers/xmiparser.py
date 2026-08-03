b8a12f887f7ace978fbfe2d3bdf5497c8a109e05
Arjen Brienen <brienen@e-space.nl>
2026-08-03 11:40:51 +0200

    perf(xmi): indexeer type-verwijzingen in plaats van per element te zoeken
    De drie afsluitende lussen van phase2_process_connectors deden per
    enumeratie, klasse en datatype een eigen XPath-descent over de volledige
    boom om verwijzingen (.//type[@xmi:idref=...]) te vinden. Op een groot
    model zijn dat ruim duizend scans van hetzelfde document.
    
    Nu wordt de boom eenmaal doorlopen en gegroepeerd op idref, waarna elke
    lus een dict-lookup doet. Daarnaast evalueert het uitlezen van de
    multipliciteiten (lowerValue/upperValue) zijn XPath nog maar een keer in
    plaats van twee, en wordt het al opgehaalde type-element hergebruikt.
    
    Import van 'Gemeentelijk Gegevensmodel 2.4.0.xml': 14,9s -> 8,0s, met
    een identieke database als resultaat.

--- crunch_uml/parsers/xmiparser.py
+++ crunch_uml/parsers/xmiparser.py
@@ -1,5 +1,6 @@
 import logging
 import re
+from collections import defaultdict
 
 import chardet
 import requests
@@ -52,6 +53,16 @@ def load_xmi(source):
         raise RuntimeError(
             f"Probleem met XMI inlezen (declared: {declared_encoding}, detected: {detected_encoding}): {e}"
         )
+
+
+def get_end_value(endpoint, tag):
+    """Waarde van het eerste kindelement ``tag`` van een association-end.
+
+    Eén XPath-evaluatie per aanroep: de vorige inline-variant evalueerde
+    dezelfde expressie tweemaal (eenmaal om te tellen, eenmaal om te lezen).
+    """
+    nodes = endpoint.xpath(f"./{tag}")
+    return nodes[0].get("value") if nodes else None
 
 
 def remove_EADatatype(input_string):
@@ -222,9 +233,6 @@ class XMIParser(Parser):
                             )
 
                         endpoint = endpoints[0]
-                        getval = lambda x, endpoint: (  # noqa
-                            endpoint.xpath(f"./{x}")[0].get("value") if len(endpoint.xpath(f"./{x}")) else None  # noqa
-                        )  # noqa
                         typenode = endpoint.xpath("./type")
                         if len(typenode) == 0:
                             clsid = util.getEAGuid()
@@ -240,19 +248,19 @@ class XMIParser(Parser):
                             else:
                                 association.dst_class_id = clsid  # type: ignore
                         else:
-                            clsid = endpoint.xpath("./type")[0].get("{" + ns["xmi"] + "}idref")
+                            clsid = typenode[0].get("{" + ns["xmi"] + "}idref")
                             cls = schema.get_class(clsid)
                             if cls is None:
                                 clazz = db.Class(id=clsid, name=const.ORPHAN_CLASS)
                                 schema.save(clazz)
                             if "src" in id:
                                 association.src_class_id = clsid  # type: ignore
-                                association.src_mult_start = str(getval("lowerValue", endpoint))  # type: ignore
-                                association.src_mult_end = str(getval("upperValue", endpoint))  # type: ignore
+                                association.src_mult_start = str(get_end_value(endpoint, "lowerValue"))  # type: ignore
+                                association.src_mult_end = str(get_end_value(endpoint, "upperValue"))  # type: ignore
                             else:
                                 association.dst_class_id = clsid  # type: ignore
-                                association.dst_mult_start = str(getval("lowerValue", endpoint))  # type: ignore
-                                association.dst_mult_end = str(getval("upperValue", endpoint))  # type: ignore
+                                association.dst_mult_start = str(get_end_value(endpoint, "lowerValue"))  # type: ignore
+                                association.dst_mult_end = str(get_end_value(endpoint, "upperValue"))  # type: ignore
                     else:
                         err = f"Association {association.name} with {association.id} has more than two endpoints. panic"
                         logger.error(err)
@@ -303,10 +311,20 @@ class XMIParser(Parser):
                 attribute.type_class_id = cls.id
             schema.save(attribute)
 
+        # Alle type-verwijzingen in één doorloop indexeren op idref. De drie
+        # lussen hierna (enumeraties, klassen, datatypes) deden elk per element
+        # een eigen XPath-descent over de volledige boom: op een groot model
+        # ruim duizend scans van hetzelfde document. Nu is het één scan plus
+        # een dict-lookup per element.
+        ns_xmi_idref = "{" + ns["xmi"] + "}idref"
+        type_refs_by_idref = defaultdict(list)
+        for type_node in node.xpath(".//type[@xmi:idref]", namespaces=ns):  # type: ignore
+            type_refs_by_idref[type_node.get(ns_xmi_idref)].append(type_node)
+
         # Last of all set enumerations
         enums = schema.get_all_enumerations()
         for enum in enums:
-            enumverws = node.xpath(".//type[@xmi:idref='" + enum.id + "']", namespaces=ns)
+            enumverws = type_refs_by_idref.get(enum.id, ())
             for enumverw in enumverws:
                 property = enumverw.getparent()
                 if property.tag == "ownedAttribute" and property.get("{" + ns["xmi"] + "}type") == "uml:Property":
@@ -320,7 +338,7 @@ class XMIParser(Parser):
         # Last of all set object references
         classes = schema.get_all_classes()
         for clazz in classes:
-            classverws = node.xpath(".//type[@xmi:idref='" + clazz.id + "']", namespaces=ns)
+            classverws = type_refs_by_idref.get(clazz.id, ())
             for classverw in classverws:
                 property = classverw.getparent()
                 if property.tag == "ownedAttribute" and property.get("{" + ns["xmi"] + "}type") == "uml:Property":
@@ -334,7 +352,7 @@ class XMIParser(Parser):
         # Last of all set object references
         datatypes = schema.get_all_datatypes()
         for dt in datatypes:
-            dtverws = node.xpath(".//type[@xmi:idref='" + dt.id + "']", namespaces=ns)
+            dtverws = type_refs_by_idref.get(dt.id, ())
             for dtverw in dtverws:
                 property = dtverw.getparent()
                 if property.tag == "ownedAttribute" and property.get("{" + ns["xmi"] + "}type") == "uml:Property":
